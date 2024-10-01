cb32f1ead21f9d010883de7d79c74d57551a3c81
GonzaloAlarconUBA <espukidmt@gmail.com>
2024-10-01 16:41:00 -0300

    Termine grilla de productos con diseño

--- Main/kioscocetec/app/components/Footer.tsx
+++ Main/kioscocetec/app/components/Footer.tsx
@@ -4,6 +4,7 @@ import { IoIosMail } from "react-icons/io";
 const Footer = () => {
     return (
         <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        
             <a
             className="flex items-center gap-2 hover:underline hover:underline-offset-4"
             href="https://www.instagram.com/cetec.uba/"
